a0280fef3abf7783639a06e882498ceb1f5d19ee
Aswin Vijayakumar <aswinvk28@gmail.com>
2020-10-23 17:44:22 +0100

    more updates

---
+++ run.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var_dump(function_exists('curl_init'));
+
+phpinfo();
+
+var_dump(get_loaded_extensions());
+
+var_dump(class_exists('NumberFormatter'));
+
+?>
---
+++ youtube.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="http://www.youtube.com/apiplayer?version=3&enablejsapi=1"></script>
+        <script src="packages/jQuery.jPlayer.2.4.0/jQuery.jPlayer.2.4.0/jquery.jplayer.js"></script>
+    </head>
+    <body>
+        <div id="player"></div>
+        <script>
+            
+        </script>
+    </body>
+</html>
